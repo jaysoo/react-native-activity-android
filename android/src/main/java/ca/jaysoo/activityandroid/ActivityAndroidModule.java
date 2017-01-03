a7e19193870f74dd9d84752fa9899a4979ed98d8
Ryan Homer <ryan@murage.ca>
2017-01-03 13:31:44 -0800

    Compatibility with react-native 0.29.0 and higher.

--- android/src/main/java/ca/jaysoo/activityandroid/ActivityAndroidModule.java
+++ android/src/main/java/ca/jaysoo/activityandroid/ActivityAndroidModule.java
@@ -15,11 +15,8 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
 public class ActivityAndroidModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
-  private Activity mCurrentActivity;
-
-  public ActivityAndroidModule(ReactApplicationContext reactContext, Activity activity) {
+  public ActivityAndroidModule(ReactApplicationContext reactContext) {
     super(reactContext);
-    mCurrentActivity = activity;
     reactContext.addLifecycleEventListener(this);
   }
 
@@ -30,7 +27,11 @@ public class ActivityAndroidModule extends ReactContextBaseJavaModule implements
 
   @ReactMethod
   public void moveTaskToBack(Callback onSuccess, Callback onError) {
-    boolean wasMoved = mCurrentActivity.moveTaskToBack(true);
+    final Activity activity = getCurrentActivity();
+
+    if (activity == null) return;
+
+    boolean wasMoved = activity.moveTaskToBack(true);
 
     if (wasMoved) {
       onSuccess.invoke();
